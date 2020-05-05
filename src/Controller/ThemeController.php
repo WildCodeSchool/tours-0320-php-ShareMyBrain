94f855852c7bc3efdc69f4e7a48b842e766583c8
DSARRAZY <d.sarrazy@gmail.com>
2020-05-05 14:05:47 +0200

    je peux afficher toutes les questions d'un thème

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\ThemeManager;
+use App\Model\QuestionManager;
 
 /**
  * Class ThemeController
@@ -24,5 +25,26 @@ class ThemeController extends AbstractController
         $themes = $themeManager->selectAll();
 
         return $this->twig->render('Theme/index.html.twig', ['themes' => $themes]);
+    }
+
+    /**
+     * Display question listing by theme
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(int $id) : string
+    {
+        $questionManager = new QuestionManager();
+        $questions = $questionManager->selectAllByTheme($id);
+
+        $themeManager = new ThemeManager();
+        $theme = $themeManager->selectOneById($id);
+
+        return $this->twig->render('Theme/show.html.twig', [
+            'questions' => $questions,
+            'theme' => $theme]);
     }
 }
--- src/Model/QuestionManager.php
+++ src/Model/QuestionManager.php
@@ -16,4 +16,22 @@ class QuestionManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    /**
+     * @param int $id
+     */
+    public function selectAllByTheme(int $id)
+    {
+        //prepared request
+        $statement = $this->pdo->prepare("SELECT question.id,name, username,content
+        FROM `$this->table`
+        JOIN theme ON theme.id=question.id_theme 
+        JOIN user ON user.id=question.id_user
+        WHERE theme.id=:id");
+
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
 }
